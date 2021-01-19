5ecac686c41b05d19236a33f046b64254ffbb70d
Yevhenii Zakharenkov <evgen.fw@gmail.com>
2021-01-19 23:01:13 +0200

    convert dict to json

--- parsebitok.py
+++ parsebitok.py
@@ -1,6 +1,6 @@
 import json
 
-with open("tests/tests.json", "r") as read_file:
+with open("tests/result.json", "r", encoding="utf8") as read_file:
     data = json.load(read_file)
 
 bitloks = {}
@@ -9,11 +9,14 @@ def parse_itlocker():
     list_messages = data["messages"]
     count = 0
     for i in list_messages:
-        if i.get("from") == "ITLocker":
-            zion_number = list_messages[count - 1]["text"]
-            password, recovery_key = list_messages[count]["text"].split("+")
-            update_dictionary(zion_number[1].replace(' ',''), password, recovery_key)
-        count += 1
+        try:
+            if i.get("from") == "ITLocker":
+                zion_number = list_messages[count - 1]["text"]
+                password, recovery_key = list_messages[count]["text"].split("+")
+                update_dictionary(zion_number[1].replace(' ',''), password, recovery_key)
+            count += 1
+        except Exception:
+            print("Text Not found")
 
 def update_dictionary(zion, passwod, recovery_key):
     bitloks["Zion"] = zion
@@ -21,3 +24,6 @@ def update_dictionary(zion, passwod, recovery_key):
     bitloks["Recovery"] = recovery_key
 
 parse_itlocker()
+
+with open("tests/result-1.json", "w") as outfile:
+    json.dump(bitloks, outfile)
