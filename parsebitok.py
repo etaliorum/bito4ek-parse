68a12be74cb9684a4be983eecc899d25b8999e8c
Yevhenii Zakharenkov <evgen.fw@gmail.com>
2021-01-20 18:55:07 +0200

    mini refactoring

--- parsebitok.py
+++ parsebitok.py
@@ -4,9 +4,10 @@ import re
 with open("tests/result.json", "r", encoding="utf8") as read_file:
     data = json.load(read_file)
 
-bitloks = {'bito4ki': []}
+bit_locks = {'bito4ki': []}
 
-def parse_itlocker():
+
+def parse_ITLocker():
     list_messages = data["messages"]
     count = 0
     for massage in list_messages:
@@ -19,31 +20,34 @@ def parse_itlocker():
 
                 split_massage = re.split(r'\+', massage["text"])
                 password, recovery_key = split_massage[0], split_massage[1]
-                dict = {'zion': zion_number,
-                        'password': password,
-                        'recovery': recovery_key}
+                gen_dict = {'zion': zion_number,
+                            'password': password,
+                            'recovery': recovery_key}
 
-                if check_considences(dict) == 0:
-                    update_dictionary(dict)
+                if check_coincidences(gen_dict) == 0:
+                    update_dictionary(gen_dict)
 
             except IndexError:
                 print('Not Found! Zion:', zion_number)
 
         count += 1
 
-def check_considences(dict):
+
+def check_coincidences(l_dict):
     status = 0
-    list_bitloks = bitloks['bito4ki']
-    for dict_l in list_bitloks:
-        if dict_l.get('zion') == dict.get('zion'):
+    list_blocks = bit_locks['bito4ki']
+    for dict_l in list_blocks:
+        if dict_l.get('zion') == l_dict.get('zion'):
             status += 1
     return status
 
-def update_dictionary(dict):
-    bitloks['bito4ki'].append(dict)
 
-parse_itlocker()
-print(len(bitloks['bito4ki']))
+def update_dictionary(l_dict):
+    bit_locks['bito4ki'].append(l_dict)
+
+
+parse_ITLocker()
+print(len(bit_locks['bito4ki']))
 
 with open("tests/result-1.json", "w") as outfile:
-    json.dump(bitloks, outfile)
+    json.dump(bit_locks, outfile)
