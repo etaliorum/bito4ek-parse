9b996b1cdec15cfabc7a9a6694ac38f8aa594fec
Yevhenii Zakharenkov <etaliorum@zion.local>
2021-01-20 14:14:59 +0200

    updated: realize parser ITLocker, next: will realize delete_considences()

--- parsebitok.py
+++ parsebitok.py
@@ -1,29 +1,41 @@
 import json
+import re
 
 with open("tests/result.json", "r", encoding="utf8") as read_file:
     data = json.load(read_file)
 
-bitloks = {}
+bitloks = []
 
 def parse_itlocker():
     list_messages = data["messages"]
     count = 0
-    for i in list_messages:
-        try:
-            if i.get("from") == "ITLocker":
-                zion_number = list_messages[count - 1]["text"]
-                password, recovery_key = list_messages[count]["text"].split("+")
-                update_dictionary(zion_number[1].replace(' ',''), password, recovery_key)
-            count += 1
-        except Exception:
-            print("Text Not found")
+    for massage in list_messages:
+        if massage.get("from") == "ITLocker":
+            prev_massage = list_messages[count - 1]["text"]
 
-def update_dictionary(zion, passwod, recovery_key):
-    bitloks["Zion"] = zion
-    bitloks["Password"] = passwod
-    bitloks["Recovery"] = recovery_key
+            try:
+                if len(prev_massage) > 1:
+                    zion_number = re.sub(r'\s', '', prev_massage[1])
+                split_massage = re.split(r'\+', massage["text"])
+                password, recovery_key = split_massage[0], split_massage[1]
+                dict = {'zion': zion_number,
+                        'poassword': password,
+                        'recovery': recovery_key}
+                update_dictionary(dict)
+            except IndexError:
+                print('Not Found! Zion:', zion_number)
+
+        count += 1
+
+def delete_considences():
+    return 0;
+
+def update_dictionary(dict):
+    bitloks.append(dict)
 
 parse_itlocker()
+delete_considences()
+#print(bitloks)
 
 with open("tests/result-1.json", "w") as outfile:
     json.dump(bitloks, outfile)
