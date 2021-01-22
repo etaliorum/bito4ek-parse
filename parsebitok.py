4991ff40ed96584811b9964af5a60cbd9dac67ea
Yevhenii Zakharenkov <evgen.fw@gmail.com>
2021-01-22 11:42:47 +0200

    Added parse without bitlocks form ITLocker

--- parsebitok.py
+++ parsebitok.py
@@ -1,13 +1,56 @@
 import json
 import re
 
-with open("tests/result.json", "r", encoding="utf8") as read_file:
-    data = json.load(read_file)
-
 bit_locks = {'bito4ki': []}
+
+def position_dict(l_list):
+    for item in range(len(l_list)):
+        if type(l_list[item]) == dict:
+            return item
+
+
+def get_atr_texts(list_messages):
+    list_m = []
+    for massage in list_messages:
+        if isinstance(massage['text'], list):
+            if massage['text'][position_dict(massage['text'])].get('text') == '/zion':
+                list_m.append(massage['text'])
+    return list_m
+
+
+def update_dictionary(l_dict):
+    bit_locks['bito4ki'].append(l_dict)
+
+
+def parse_without_ITLocker():
+    with open("tests/result-without-itlocker.json", "r", encoding="utf8") as read_file:
+        data = json.load(read_file)
+    lists_texts = get_atr_texts(data["messages"])
+
+    for item in lists_texts:
+        for string_l in item:
+            if type(string_l) != dict:
+                zion = re.findall(r'[lL]\-\d{3}|\b\d{3}\b', string_l)
+                password = re.findall(r'\d{7}', string_l)
+                recovery_key = re.findall(r'\d{6}-\d{6}-\d{6}-\d{6}-\d{6}-\d{6}-\d{6}-\d{6}', string_l)
+
+                zion = ''.join(zion)
+                password = ''.join(password)
+                recovery_key = ''.join(recovery_key)
+
+                gen_dict = {'zion': zion,
+                            'password': password,
+                            'recovery': recovery_key}
+                update_dictionary(gen_dict)
+
+    with open("tests/result-2.json", "w") as outfile:
+        json.dump(bit_locks, outfile, indent=2)
 
 
 def parse_ITLocker():
+    with open("tests/result.json", "r", encoding="utf8") as read_file:
+        data = json.load(read_file)
+
     list_messages = data["messages"]
     count = 0
     for massage in list_messages:
@@ -32,6 +75,9 @@ def parse_ITLocker():
 
         count += 1
 
+    with open("tests/result-1.json", "w") as outfile:
+        json.dump(bit_locks, outfile, indent=2)
+
 
 def check_coincidences(l_dict):
     status = 0
@@ -46,8 +92,8 @@ def update_dictionary(l_dict):
     bit_locks['bito4ki'].append(l_dict)
 
 
-parse_ITLocker()
-print(len(bit_locks['bito4ki']))
+if __name__ == "__main__":
+    parse_ITLocker()
+    parse_without_ITLocker()
 
-with open("tests/result-1.json", "w") as outfile:
-    json.dump(bit_locks, outfile)
+
